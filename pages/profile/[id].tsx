ac6acd2306f8ee4bf13ec8e0366c61850d1058c4
Kristina Gorbunova <kristina.gorbunova1997@gmail.com>
2022-05-12 16:08:00 +0800

    added Post icons; user profile page

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -1,5 +1,6 @@
 import {Paper, Avatar, Typography, Button, Tabs, Tab} from "@material-ui/core";
 import {
+  Settings,
   SettingsOutlined as SettingsIcon,
   TextsmsOutlined as MessageIcon
 } from "@material-ui/icons";
@@ -10,6 +11,29 @@ export default function Profile() {
   return (
     <MainLayout contentFullWidth hideComments>
       <Paper className="pl-20 pr-20 pt-20 mb-30" elevation={0}>
+        <div className="d-flex justify-between">
+          <div>
+            <Avatar
+              style={{width: 120, height: 120, borderRadius: 6}}
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsmr1u2RAzYkqgGepYTCIReEOkqGLHqguw3Q&usqp=CAU"
+            />
+            <Typography style={{fontWeight: 'bold'}} className="mt-10" variant="h4">
+              Amon Bower
+            </Typography>
+          </div>
+          <div>
+            <Button
+              style={{height: 42, minWidth: 45, width: 45, marginRight: 10}}
+              variant="contained"
+            >
+              <SettingsIcon/>
+            </Button>
+            <Button style={{height: 42}} variant="contained" color="primary">
+              <MessageIcon className="mr-10"/>
+              Message
+            </Button>
+          </div>
+        </div>
 
       </Paper>
     </MainLayout>
