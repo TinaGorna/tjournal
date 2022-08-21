deb165f3c0eb26964cc8055eb66bf1d4cba42cb4
Kris <77728617+TinaGorna@users.noreply.github.com>
2022-08-21 17:12:19 +0300

    FullPost component SCSS

--- components/FullPost/index.tsx
+++ components/FullPost/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Paper, Typography} from "@material-ui/core";
 import styles from "./FullPost.module.scss"
+import {PostActions} from "../PostActions";
 
 export const FullPost = () => {
     return (
@@ -9,32 +10,49 @@ export const FullPost = () => {
                 margin: "0 auto",
                 width: 680,
             }}>
-                <Typography variant="h4"
-                            className={styles.title}>
-                    Sega снимет полнометражные фильмы по двум играм 1990-х годов
-                </Typography>
-                <Typography>
-                    <a>
-                        После успеха «Соника в кино» компания решила экранизировать музыкальную игру Space Channel 5
-                        и
-                        аркадный экшн Comix Zone, сообщил THR.
-                    </a>
+                <div>
+                    <Typography variant="h4"
+                                className={styles.title}>
+                        Sega снимет полнометражные фильмы по двум играм 1990-х годов
+                    </Typography>
                     <Typography>
                         <a>
-                            Фильм расскажет историю незадачливого работника фаст-фуда, которого репортер из будущего
-                            завербовал
-                            с целью спасти мир от инопланетян.
+                            После успеха «Соника в кино» компания решила экранизировать музыкальную игру Space Channel 5
+                            и
+                            аркадный экшн Comix Zone, сообщил THR.
                         </a>
+                        <Typography>
+                            <a>
+                                Фильм расскажет историю незадачливого работника фаст-фуда, которого репортер из будущего
+                                завербовал
+                                с целью спасти мир от инопланетян.
+                            </a>
+                        </Typography>
+                        <Typography>
+                            Лента, основанная на Comix Zone, расскажет о молодом энергичном писателе и измученном
+                            создателе
+                            комиксов, оказавшихся втянутыми в последний выпуск своего творения.
+                        </Typography>
+                        <Typography>
+                            Сценарий пишет Мэй Кэтт («Юная Лига Справедливости», «Драконы: Девять миров»).
+                        </Typography>
                     </Typography>
-                    <Typography>
-                        Лента, основанная на Comix Zone, расскажет о молодом энергичном писателе и измученном
-                        создателе
-                        комиксов, оказавшихся втянутыми в последний выпуск своего творения.
-                    </Typography>
-                    <Typography>
-                        Сценарий пишет Мэй Кэтт («Юная Лига Справедливости», «Драконы: Девять миров»).
-                    </Typography>
-                </Typography>
+                    <div
+                        style={{
+                            width: 250
+                        }}
+                    >
+                        <PostActions/>
+                    </div>
+                    <div
+                    className={styles.userInfo}
+                    >
+                        <img
+                            src="https://coolsen.ru/wp-content/uploads/2021/06/14-4.jpg" alt="Avatar"/>
+                        <b>Donnie Darko</b>
+                        <span>+1685</span>
+                    </div>
+                </div>
             </div>
         </Paper>
     )
--- components/PostActions.tsx
+++ components/PostActions.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {CSSProperties} from "react";
 import {IconButton} from "@material-ui/core";
 import {
     BookmarkBorderOutlined as FavoriteIcon,
@@ -7,14 +7,14 @@ import {
     ShareOutlined as ShareIcon
 } from "@material-ui/icons";
 
-const styles = {
+const styles: CSSProperties = {
     "display": "flex",
-    "justify-content": "space-between",
+    "justifyContent": "space-between",
     "position": "relative",
-    "top": 5,
-    "list-style": "none",
-    "padding": 0,
-    "margin": 0,
+    "top": "5",
+    "listStyle": "none",
+    "padding": "0",
+    "margin": "0",
 }
 export const PostActions: React.FC = () => {
     return (
