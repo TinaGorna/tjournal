498171d582a89938eb2130140529b65f205da31e
Kris <77728617+TinaGorna@users.noreply.github.com>
2022-08-21 20:20:45 +0300

    slug.tsx

--- components/CommentPost/index.tsx
+++ components/CommentPost/index.tsx
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
-
+import {IconButton, Menu, Paper, Typography} from "@material-ui/core";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import {text} from "stream/consumers";
 
 interface CommentPostProps {
     user: {
@@ -22,7 +24,25 @@ export const CommentPost: React.FC<CommentPostProps> = () => {
     }
     return (
         <div>
+            <Paper elevation={0} className="p-20" classes={{root: styles.paper}}>
+                <Typography variant="h6" classes={styles.title}>
+                    <a href="#">{post.title}</a>
+                    <IconButton onClick={handleClick}>
+                        <MoreVertIcon/>
+                    </IconButton>
+                </Typography>
+                <Typography className="mt-10 mb-15">
+                    {text}
+                </Typography>
 
+                <Menu
+                    id="simple-menu"
+                    anchorEl={anchorEl}
+                >
+
+                </Menu>
+
+            </Paper>
         </div>
     )
 }
--- pages/news/[slug].tsx
+++ pages/news/[slug].tsx
@@ -1,11 +1,20 @@
 import * as React from "react"
 import {MainLayout} from "../../layouts/MainLayout";
 import {FullPost} from "../../components/FullPost";
+import {Divider, Paper, Tab, Tabs, Typography} from "@material-ui/core";
 
 export default function Home() {
     return (
         <MainLayout contentFullWidth>
             <FullPost/>
+            <Paper elevation={0} className="mt-40 p-30">
+                <Typography variant="h6" className="mb-20">42 comments</Typography>
+                <Tabs className="mt-20" value={0} indicatorColor="primary" textColor="primary">
+                    <Tab label="Популярные"/>
+                    <Tab label="По порядку"/>
+                </Tabs>
+                <Divider/>
+            </Paper>
         </MainLayout>
     )
 }
