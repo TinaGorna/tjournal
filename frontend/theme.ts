411b6ec4d317c0fc8ea9365ba9b7d7d987c51b3d
Kris <77728617+TinaGorna@users.noreply.github.com>
2022-09-03 18:23:06 +0300

    Add files via upload

---
+++ frontend/theme.ts
@@ -0,0 +1,54 @@
+import {createTheme} from "@material-ui/core";
+
+export const theme = createTheme({
+    props: {
+        MuiButtonBase: {
+            disableRipple: true
+        }
+    },
+    palette: {
+        primary: {
+            main: "#4683d9"
+        }
+    },
+    overrides: {
+        MuiPaper: {
+            rounded: {
+                borderRadius: 8
+            }
+        },
+        MuiDialog: {
+            paper: {
+                boxShadow: "none"
+            }
+        },
+        MuiButton: {
+            root: {
+                borderRadius: "10px",
+                textTransform: "inherit",
+                fontSize: 16,
+                transition: "none",
+                "&:active": {
+                    boxShadow:
+                        "0 1px 1px rgba(0 0 0 / 15%), 0 4px 7px rgba(0 0 0 / 15%)",
+                    transform: "translateY(1px)"
+                }
+            },
+            contained: {
+                backgroundColor: "white",
+                boxShadow: "0 1px 1px rgb(0 0 0 / 15%), 0 4px 7px rgb(0 0 0 / 5%), 0 -1px 0 rgb(0 0 0 / 5%), -1px 0 0 rgb(0 0 0 / 5%), 1px 0 0 rgb(0 0 0 / 5%)",
+                "&:hover": {
+                    backgroundColor: "white",
+                    boxShadow: "0 1px 1px rgb(0 0 0 / 15%), 0 4px 7px rgb(0 0 0 / 5%), 0 -1px 0 rgb(0 0 0 / 5%), -1px 0 0 rgb(0 0 0 / 5%), 1px 0 0 rgb(0 0 0 / 5%)",
+
+                }
+            },
+            containedPrimary: {
+                backgroundColor: "#4683d9",
+                "&:hover": {
+                    backgroundColor: "#437CCE"
+                }
+            }
+        }
+    }
+})
